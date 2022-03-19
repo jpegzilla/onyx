43e235857d1b67287f2bbfd8120ce3acab1f472d
eris <eris@jpegzilla.com>
2022-03-19 17:53:08 -0400

    user preference / color scheme stuff

--- js/main.mjs
+++ js/main.mjs
@@ -6,15 +6,47 @@ import {
 } from './utils/index.mjs'
 import { LoadingScreen } from './components/index.mjs'
 
-console.log('hello from the main file!')
-
 export const minerva = new Minerva('jpegzilla-onyx')
 export const arachne = Arachne
 
-console.log({ colorSchemeToUse, translationToUse })
+const setupUserPrefs = (minerva, _arachne) => {
+  console.log(
+    { colorSchemeToUse, translationToUse },
+    minerva.get('colorScheme'),
+    minerva.get('language')
+  )
+
+  const { highlight } = minerva.get('systemColors')
+
+  const setColorScheme = colorScheme => {
+    document.documentElement.classList.add(colorScheme)
+  }
+
+  const setLanguage = language => {
+    document.documentElement.setAttribute('lang', language)
+  }
+
+  setColorScheme(minerva.get('colorScheme'))
+  setLanguage(minerva.get('language'))
+
+  minerva.on('language', setColorScheme)
+  minerva.on('colorScheme', setLanguage)
+
+  if (!minerva.get('colorScheme')) {
+    minerva.set('colorScheme', colorSchemeToUse)
+  }
+
+  if (!minerva.get('language')) {
+    minerva.set('language', translationToUse)
+  }
+
+  document.documentElement.style.setProperty('--hl-color-override', highlight)
+}
 
 const elements = [LoadingScreen]
 
 elements.forEach(({ name, element }) => {
   if (name && element) customElements.define(name, element)
 })
+
+setupUserPrefs(minerva)
