39843458d95059d1fe31f94ee158e68e32aefbff
e <eris@jpegzilla.com>
2022-04-09 17:18:11 -0400

    fixed up component exports

--- js/components/index.mjs
+++ js/components/index.mjs
@@ -1,17 +1,17 @@
 import LoadingScreen from './loadingScreen.mjs'
-import Box from './box.mjs'
 import Header from './header.mjs'
 import Logo from './logo.mjs'
 import ColorDisplay from './colorDisplay.mjs'
 import DataDisplay from './dataDisplay.mjs'
 import Footer from './footer.mjs'
+import Controls from './controls.mjs'
 
 export default [
   LoadingScreen,
-  Box,
   Header,
   Logo,
   ColorDisplay,
   DataDisplay,
   Footer,
+  Controls,
 ]
