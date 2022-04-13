6796e94927ce6589f1bf0063c1fa0048802d9c30
e <eris@jpegzilla.com>
2022-04-13 13:12:55 -0400

    added limitedList util

---
+++ js/utils/dataStructures/limitedList.mjs
@@ -0,0 +1,33 @@
+/**
+ * a list that can only be a certain length before starting to
+ * remove the oldest items.
+ */
+class LimitedList {
+  /**
+   * creates a new limited list.
+   * @param {number} limit  the maximum amount of items to hold
+   */
+  constructor(limit) {
+    this.limit = limit
+
+    this.items = []
+  }
+
+  add(item) {
+    if (this.items.length >= this.limit) {
+      this.items.shift()
+    }
+
+    this.items.push(item)
+
+    return this.items
+  }
+
+  clear() {
+    this.items = []
+
+    return this.items
+  }
+}
+
+export default LimitedList
