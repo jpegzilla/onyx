6c2184d819291447a7295899808d3784af9b6ac6
e <eris@jpegzilla.com>
2022-06-18 19:36:54 -0400

    barebones beginning of controls

--- js/components/controls.mjs
+++ js/components/controls.mjs
@@ -1,5 +1,11 @@
 import Component from './component.mjs'
-import { html } from './../utils/index.mjs'
+import {
+  html,
+  LimitedList,
+  throttle,
+  setCustomProperty,
+} from './../utils/index.mjs'
+import { minerva } from './../main.mjs'
 
 class Controls extends Component {
   static name = 'onyx-controls'
@@ -8,10 +14,36 @@ class Controls extends Component {
     super()
 
     this.id = Controls.name
+    this.history = new LimitedList(minerva.get('historySize'))
+    this.colors = minerva.get('colors')
+    this.activeColor = 'fg'
   }
 
+  updateHistory() {
+    const update = throttle(() => {
+      // this.history.add()
+    }, 200)
+
+    update()
+  }
+
+  setForeground() {}
+
+  setBackground() {}
+
   connectedCallback() {
-    this.innerHTML = html`<div>controls</div>`
+    setCustomProperty('--color-display-color', minerva.get('colors').fg)
+    setCustomProperty('--color-display-background', minerva.get('colors').bg)
+
+    this.controlInputs = this.querySelectorAll('.color-control')
+
+    this.innerHTML = html`<section>
+      <header></header>
+      <div class="controls-container">
+        <div class="controls-left"></div>
+        <div class="controls-right"></div>
+      </div>
+    </section>`
   }
 }
 
