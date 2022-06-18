a392c6f52d31428b10bac4c859f4260dc42a8a5b
e <eris@jpegzilla.com>
2022-06-18 19:02:06 -0400

    updated colordisplay

--- js/components/colorDisplay.mjs
+++ js/components/colorDisplay.mjs
@@ -1,5 +1,5 @@
 import Component from './component.mjs'
-import { html, setCustomProperty } from './../utils/index.mjs'
+import { html } from './../utils/index.mjs'
 import { minerva } from './../main.mjs'
 
 const conversionWorker = './js/workers/colorConversion.worker.mjs'
@@ -11,17 +11,32 @@ class ColorDisplay extends Component {
     super()
 
     this.id = ColorDisplay.name
-    this.colors = minerva.get('colors')
-    this.activeColor = 'fg'
-
+    this.activeColor = minerva.get('activeColor') || 'bg'
     this.conversionWorker = new Worker(conversionWorker, { type: 'module' })
   }
 
-  updateColors(fg, bg) {
-    this.querySelector('.foreground-color-value').textContent = fg
-    this.querySelector('.background-color-value').textContent = bg
+  updateReadout({ fg, bg }) {
+    console.log(fg, bg)
+    const readout = this.querySelector('.color-display-readout span')
 
-    this.getConversions(fg)
+    if (this.activeColor === 'bg') {
+      readout.textContent = bg
+    }
+
+    if (this.activeColor === 'fg') {
+      readout.textContent = fg
+    }
+
+    this.colors = {
+      fg,
+      bg,
+    }
+  }
+
+  updateColors({ fg, bg }) {
+    this.updateReadout({ fg, bg })
+
+    // this.getConversions(fg)
   }
 
   getConversions(hexColor) {
@@ -38,33 +53,69 @@ class ColorDisplay extends Component {
   }
 
   connectedCallback() {
-    setCustomProperty('--color-display-color', minerva.get('colors').fg)
-
     this.innerHTML = html`
       <section class="color-display-container">
-        <div class="hex-value-foreground">
-          <div class="foreground-color-name">foreground</div>
-          <div class="foreground-color-value">#000000</div>
-        </div>
+        <div class="color-display-readout"><span>#000000</span></div>
+        <div class="color-display-selector">
+          <div class="color-display-selector-description">
+            <span>current color readout:</span>
+          </div>
 
-        <div class="hex-value-background">
-          <div class="background-color-name">background</div>
-          <div class="background-color-value">#000000</div>
-        </div>
+          <button class="display-background-color" data-color="bg">
+            <span>${this.activeColor === 'bg' ? '> ' : ''}background</span>
+          </button>
 
-        <div class="color-display-conversions">
-          <div class="conversion-rgba"></div>
-          <div class="conversion-hsla"></div>
-          <div class="conversion-hwb"></div>
-          <div class="conversion-lab"></div>
-          <div class="conversion-lch"></div>
-          <div class="conversion-xyz"></div>
+          <button class="display-text-color" data-color="fg">
+            <span>${this.activeColor === 'fg' ? '> ' : ''}text</span>
+          </button>
         </div>
       </section>
     `
 
+    const displayBackgroundColor = this.querySelector(
+      '.display-background-color'
+    )
+    const displayTextColor = this.querySelector('.display-text-color')
+
+    const buttons = [displayBackgroundColor, displayTextColor]
+
+    buttons.forEach((element, _idx, arr) => {
+      element.addEventListener('click', e => {
+        const activeColor = e.currentTarget.dataset.color
+        const text = e.currentTarget.querySelector('span').textContent
+
+        if (activeColor === this.activeColor) {
+          return
+        }
+
+        e.currentTarget.querySelector('span').textContent = `> ${text}`
+
+        arr
+          .filter(
+            elem => !elem.classList.contains(e.currentTarget.classList[0])
+          )
+          .forEach(e => {
+            const textContent = e.querySelector('span').textContent
+
+            e.querySelector('span').textContent = textContent
+              .replace('>', '')
+              .trim()
+          })
+
+        minerva.set('activeColor', activeColor)
+      })
+    })
+
+    minerva.on('activeColor', color => {
+      this.activeColor = color
+      this.updateReadout({
+        fg: minerva.get('colors').fg,
+        bg: minerva.get('colors').bg,
+      })
+    })
+
     minerva.on('colors', ({ fg, bg }) => {
-      this.updateColors(fg, bg)
+      this.updateColors({ fg, bg })
     })
   }
 }
