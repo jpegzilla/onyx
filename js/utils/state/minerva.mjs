544576aa1179b2cfbb8cf210f09fec876ebcc0b3
e <eris@jpegzilla.com>
2022-06-18 18:19:35 -0400

    slight refactoring / commenting to minerva

--- js/utils/state/minerva.mjs
+++ js/utils/state/minerva.mjs
@@ -3,25 +3,52 @@
 import AudioManager from './../managers/audioManager.mjs'
 import arachne from './../arachne.mjs'
 
+const PROCESS_TYPE = 'processType'
+const MACHINE = 'machine'
+const MUTE = 'mute'
+
 /**
  * Minerva - state manager!
  */
 class Minerva {
   static _store = window.localStorage
 
+  /**
+   * default settings for minerva
+   *
+   * @type {object}
+   * @property {object} volume settings for application volume
+   * @property {string} timeFormat time format to display, 12 or 24hr
+   * @property {object} systemColors colors to use for highlights
+   * @property {object} colors ??? what was this for again?
+   * @property {array}  palettes array of palette objects
+   * @property {string} machine name of instance (for data export)
+   * @property {boolean} mute true if sound should be muted
+   * @property {boolean} newPlayer true if onyx hasn't been used
+   * @property {number} arrivals false if onyx has been used before
+   * @property {string} processType ??? don't remember what this was
+   * @property {number} historySize amount of previous color storage
+   * @property {string} activeColor current color being displayed
+   */
   static defaultSettings = {
     volume: { effect: 100 },
     timeFormat: '24hr',
     systemColors: { highlight: '#e0005d' },
     colors: {},
     palettes: [],
-    machine: 'onyx', // 4 character name of workspace, customizable by user
-    mute: false,
+    [MACHINE]: 'onyx',
+    [MUTE]: false,
     newPlayer: true,
     arrivals: 0,
-    processType: 'standard',
+    [PROCESS_TYPE]: 'standard',
+    historySize: 100,
+    activeColor: 'bg',
   }
 
+  /**
+   * instantiates a minerva object
+   * @param {string} ident  unique identifier for minerva
+   */
   constructor(ident) {
     if (ident.replace(/\s/gi, '').trim() != ident) {
       arachne.error('invalid minerva key format')
@@ -42,19 +69,19 @@ class Minerva {
   }
 
   get workspaceName() {
-    return this.get('machine')
+    return this.get(MACHINE)
   }
 
   set workspaceName(name) {
-    this.set('machine', name.toLowerCase())
+    this.set(MACHINE, name.toLowerCase())
   }
 
   get workspaceType() {
-    return this.get('processType')
+    return this.get(PROCESS_TYPE)
   }
 
   set workspaceType(name) {
-    this.set('processType', name.toLowerCase())
+    this.set(PROCESS_TYPE, name.toLowerCase())
   }
 
   get audio() {
@@ -82,7 +109,7 @@ class Minerva {
     this.hasAllSounds &&
       this.store.audio &&
       this.audioManager.play(sound, {
-        mute: this.get('mute'),
+        mute: this.get(MUTE),
       })
   }
 
