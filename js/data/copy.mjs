cb1dfc653d794cb0ca1ad3e71f12c611a6944b65
e <eris@jpegzilla.com>
2022-09-14 17:23:53 -0400

    added theme switcher buttons to footer (non functional)

--- js/data/copy.mjs
+++ js/data/copy.mjs
@@ -1,10 +1,12 @@
 export const footerCopy = {
   export:
-    'exports all your palettes and settings as a .json file for you to download.',
+    'export all your palettes and settings as a .json file for you to download.',
   import:
-    'allows you to upload a .json file containing palettes and settings that you (or someone else) previously exported.',
-  save: 'allows you to locally save all current palettes and settings to your color workspace library.',
-  switch: 'allows you to switch between your saved color workspaces.',
+    'upload a .json file containing palettes and settings that you (or someone else) previously exported.',
+  save: 'locally save all current palettes and settings to your color workspace library.',
+  switch: 'switch between your saved color workspaces.',
   workspace: 'the name of the current color workspace.',
   workspaceType: 'the type of the current color workspace.',
+  dark: "set onyx's interface theme to darker colors.",
+  light: "set onyx's interface theme to lighter colors.",
 }
