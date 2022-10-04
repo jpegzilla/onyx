85eca0d02176b3332d1d1fdd55bccf512360574c
e <eris@jpegzilla.com>
2022-10-03 21:23:21 -0400

    fixed complimentary color calculation

--- js/utils/color/harmonies.mjs
+++ js/utils/color/harmonies.mjs
@@ -1,9 +1,12 @@
-import { HEX_WHITE } from './constants.mjs'
+import { hexToHSLA, hslToRGB, rgbaToHex } from './conversions.mjs'
 
 export const getComplementaryColor = hex => {
-  const inputHex = parseInt(hex.replace(/^#/, ''), 16)
+  const { h, s, l } = hexToHSLA('#1ecbe1')
+  const rotatedH = h + 180 < 360 ? h + 180 : h + 180 - 360
 
-  const outputHex = (HEX_WHITE - inputHex).toString(16).padStart(6, 0)
+  const rgb = hslToRGB(rotatedH / 360, parseInt(s) / 100, parseInt(l) / 100)
+
+  const outputHex = rgbaToHex(rgb)
 
   return `#${outputHex}`
 }
