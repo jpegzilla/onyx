ac22e9d7e55beafabef711c76cd7a40ce466c6e6
e <eris@jpegzilla.com>
2022-10-03 21:25:17 -0400

    added basic controls (that don't work yet)

--- js/components/controls.mjs
+++ js/components/controls.mjs
@@ -5,7 +5,35 @@ import {
   throttle,
   setCustomProperty,
 } from './../utils/index.mjs'
+import {
+  hexToHSLA,
+  hslToRGB,
+  rgbaToHex,
+  hexToRGBA,
+} from './../utils/color/conversions.mjs'
 import { minerva } from './../main.mjs'
+
+const MODE_RGB = 'rgb'
+const RGB_SETTINGS = {
+  name: MODE_RGB,
+  names: ['red', 'green', 'blue'],
+  ranges: [
+    [0, 255],
+    [0, 255],
+    [0, 255],
+  ],
+}
+
+const MODE_HSL = 'hsl'
+const HSL_SETTINGS = {
+  name: MODE_HSL,
+  names: ['hue', 'saturation', 'lightness'],
+  ranges: [
+    [0, 360],
+    [0, 100],
+    [0, 100],
+  ],
+}
 
 class Controls extends Component {
   static name = 'onyx-controls'
@@ -17,6 +45,17 @@ class Controls extends Component {
     this.history = new LimitedList(minerva.get('historySize'))
     this.colors = minerva.get('colors')
     this.activeColor = 'fg'
+    this.settings = {
+      [MODE_RGB]: RGB_SETTINGS,
+      [MODE_HSL]: HSL_SETTINGS,
+    }
+    this.controlMode = this.settings[MODE_HSL]
+    this.mode = MODE_HSL
+  }
+
+  setControlMode(mode) {
+    this.controlMode = this.settings[mode]
+    this.mode = mode
   }
 
   updateHistory() {
@@ -27,23 +66,123 @@ class Controls extends Component {
     update()
   }
 
+  getColors() {
+    const { bg, fg } = minerva.get('colors')
+
+    return {
+      hex: {
+        bg,
+        fg,
+      },
+      [MODE_HSL]: {
+        bg: hexToHSLA(bg),
+        fg: hexToHSLA(fg),
+      },
+      [MODE_RGB]: {
+        bg: hexToRGBA(bg),
+        fg: hexToRGBA(fg),
+      },
+    }
+  }
+
   setForeground() {}
 
   setBackground() {}
+
+  getColorAndValue(layer, index) {
+    const controlName = this.controlMode.names[index]
+
+    const colorValue = Object.values(this.getColors()[this.mode][layer])[
+      index
+    ].toString()
+
+    return `${controlName} ${colorValue}`
+  }
+
+  // use layer param to decide wether to give this html
+  // to the foreground or background sliders
+  renderSliders(layer) {
+    if ([MODE_HSL, MODE_RGB].includes(this.mode)) {
+      return this.settings[this.mode].ranges
+        .map((range, idx) => {
+          const colorValueNumber = Object.values(
+            this.getColors()[this.mode][layer]
+          )
+            [idx].toString()
+            .replaceAll(/[^0-9\.]/gi, '')
+
+          const colorAndValue = this.getColorAndValue(layer, idx)
+
+          return html`
+            <div class="slider-container">
+              <span class="color-value-${layer}-${idx}">${colorAndValue}</span>
+              <input
+                class="color-control"
+                type="range"
+                min="${range[0]}"
+                max="${range[1]}"
+                step="0.01"
+                value="${colorValueNumber}"
+              />
+            </div>
+          `
+        })
+        .join('')
+    }
+  }
 
   connectedCallback() {
     setCustomProperty('--color-display-color', minerva.get('colors').fg)
     setCustomProperty('--color-display-background', minerva.get('colors').bg)
 
-    this.controlInputs = this.querySelectorAll('.color-control')
+    console.log(this.getColors())
+    console.log(minerva)
 
     this.innerHTML = html`<section>
-      <header></header>
       <div class="controls-container">
-        <div class="controls-left"></div>
-        <div class="controls-right"></div>
+        <div class="controls-container-buttons">
+          <div>COCK</div>
+        </div>
+        <div class="controls-container-sliders">
+          <div class="controls-left">
+            <div class="controls-header">
+              <button class="lock-colors-bg">🔒</button> background color
+            </div>
+            <div class="controls-sliders background">
+              ${this.renderSliders('bg')}
+            </div>
+          </div>
+
+          <div class="controls-right">
+            <div class="controls-header">
+              <button class="lock-colors-fg">🔒</button> text color
+            </div>
+            <div class="controls-sliders foreground">
+              ${this.renderSliders('fg')}
+            </div>
+          </div>
+        </div>
       </div>
     </section>`
+
+    this.backgroundColorInputs = this.querySelectorAll(
+      '.background .color-control'
+    )
+    this.foregroundColorInputs = this.querySelectorAll(
+      '.foreground .color-control'
+    )
+
+    this.backgroundColorInputs.forEach((input, index) => {
+      input.addEventListener('input', e => {
+        console.log('background input', index, e.target.value)
+      })
+    })
+
+    this.foregroundColorInputs.forEach((input, index) => {
+      input.addEventListener('input', e => {
+        console.log('foreground input', index, e.target.value)
+      })
+    })
   }
 }
 
