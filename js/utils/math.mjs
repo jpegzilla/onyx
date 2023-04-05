ef5e29098b077018be1ce5cf1955a2ee8f966a67
e <eris@jpegzilla.com>
2023-04-05 15:18:58 -0400

    added a floatToHex and clamp function

--- js/utils/math.mjs
+++ js/utils/math.mjs
@@ -1,3 +1,9 @@
+/**
+ * normalize an array of numbers to a given range
+ * @param  {array} set            array of numbers to normalize
+ * @param  {array} [range=[0, 1]] range to normalize to
+ * @return {array}                normalized set
+ */
 export const normalize = (set, range = [0, 1]) => {
   if (range.length > 2 || !Array.isArray(set) || !Array.isArray(range))
     throw new Error('invalid arguments to normalize')
@@ -16,3 +22,20 @@ export const normalize = (set, range = [0, 1]) => {
 
   return newSet.map(n => n * newRange + initial)
 }
+
+/**
+ * convert a decimal floating-point number to 32 bit hexadecimal
+ * @param  {number} float number to convert
+ * @return {array}        array of hex bytes
+ */
+export const floatToHex = float => {
+  const view = new DataView(new ArrayBuffer(4))
+  view.setFloat32(0, float)
+
+  return new Array(4)
+    .fill()
+    .map((_, i) => view.getUint8(i).toString(16).toUpperCase().padStart(2, '0'))
+    .join(' ')
+}
+
+export const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
